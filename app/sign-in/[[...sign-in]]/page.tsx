8f2f45a9413542ef977acbfeccb54bae37fddad3
zpuckeridge <zacchary@puckeridge.me>
2023-10-13 06:54:40 +0000

    style: centered sign-in/up forms

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="mx-auto my-10">
+      <SignIn />
+    </div>
+  );
 }
